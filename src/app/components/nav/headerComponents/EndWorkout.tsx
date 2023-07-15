f4d2c8c42ba6c222f6fdfdfafde2ccdb79e86d48
Kaedin Schouten <skaedin@gmail.com>
2023-07-15 18:17:19 +0200

    v0.0.1:  - Moved credentials, api url to enviornment file  - Added Sentry integration  - Added Cognito integration  - Added base functionality  - Added Graphql Codegen  - Added Xcode Prod scheme  - Configured ESLint

---
+++ src/app/components/nav/headerComponents/EndWorkout.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import {Button} from 'react-native';
+
+interface EndWorkoutProps {
+  label: string;
+  color: string;
+  onPress: () => void;
+}
+
+const EndWorkout: React.FC<EndWorkoutProps> = props => {
+  return (
+    <Button title={props.label} color={props.color} onPress={props.onPress} />
+  );
+};
+
+export default EndWorkout;
