f7fd650911ea6e4310b06f47809f5a78c61dd11a
Kaedin Schouten <skaedin@gmail.com>
2023-07-16 23:32:32 +0200

    ### Added
    - #7: Max Lengths to textual input fields

--- src/app/utils/Constants.ts
+++ src/app/utils/Constants.ts
@@ -15,6 +15,8 @@ const Constants = {
   WEIGHT_POINTS: Array.from(new Array(400), (val, index) => `${index + 1}`),
   WEIGHT_FRACTION_POINTS: [0, 0.25, 0.5, 0.75],
   CONTAINER_PADDING: 10,
+
+  TEXT_INPUT_MAX_LENGTH: 46,
 };
 
 export default Constants;
